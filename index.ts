cb50adb29fd394f1059100ab3c9a60a3960b6bed
ifraaslam123 <ifram.aslam568@gmail.com>
2024-04-17 21:16:58 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+const currency :any = {
+    USD: 1, // Base Currency
+    EUR: 0.91,
+    GBP:0.75,
+    INR: 74.57,
+    PKR: 280,
+};
+
+let user_Ans =await inquirer.prompt([
+    {
+        name : "from",
+        message : "enter from currency",
+        type : "list",
+        choices : [`USD`, `EUR`,`GBP` , `INR`, `PKR`]
+    },
+    {
+        name : "to",
+        message : "enter to currency",
+        type : "list",
+        choices : [`USD`, `EUR`,`GBP` , `INR`, `PKR`]
+    },
+    {
+        name : "amount",
+        type : "number",
+        message : "enter your amount",
+    }
+]);
+let fromAmount = currency[user_Ans.from]
+let toAmount = currency[user_Ans.to]
+let amount =  user_Ans.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount);
+
+
